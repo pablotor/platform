adad80070c635ee3bf114fdb785a70f4668740ad
pablotor <me@pablotor.dev>
2026-09-08 19:57:12 +0300

    feat: added post edit page

---
+++ apps/web/app/(withNavSidebar)/(authenticated)/dashboard/posts/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import '@repo/ui/tiptap.css';
+
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { headers } from 'next/headers';
+
+import { postByIdQueryOptions } from '../../../../../../../lib/queryOptions/postsOptions';
+import ClientSideEditPost from './clientSideEditPost';
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+const EditPostPage = async ({ params }: PageProps) => {
+  const { id } = await params;
+  const reqHeaders = await headers();
+  const queryClient = new QueryClient();
+  const post = await queryClient.query(
+    postByIdQueryOptions(id, { headers: reqHeaders }),
+  );
+
+  return (
+    <div className="mx-auto max-w-6xl px-6 py-10 h-full">
+      <div className="flex flex-1 h-full gap-8">
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <ClientSideEditPost post={post} />
+        </HydrationBoundary>
+      </div>
+    </div>
+  );
+};
+
+export default EditPostPage;
