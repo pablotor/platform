ac028270c2e195406f2f94ead47c1c0903a05b49
pablotor <me@pablotor.dev>
2026-09-08 19:42:32 +0300

    feat: updated postsOptions with contract and endpoints

--- apps/web/lib/queryOptions/postsOptions.ts
+++ apps/web/lib/queryOptions/postsOptions.ts
@@ -1,4 +1,11 @@
-import { CreatePost, PostQuery, PostResponse } from '@repo/contracts';
+import {
+  CreatePostPayload,
+  PostQueryPayload,
+  PostQueryResponse,
+  PostResponse,
+  PostStatus,
+  UpdatePostPayload,
+} from '@repo/contracts';
 import { Toast } from '@repo/ui/toast/handler';
 import { mutationOptions, queryOptions } from '@tanstack/react-query';
 
@@ -8,31 +15,53 @@ import getQueryClient from '../queryClient';
 // key definition avoids typo issues
 const postsKey = 'posts';
 
-export const postsQueryOptions = (filters: Partial<PostQuery> = {}) => {
-  const fetchPosts = ({ authorId, order }: Partial<PostQuery>) =>
+export const postByIdQueryOptions = (
+  id: string,
+  options: { headers?: HeadersInit } = {},
+) => {
+  const fetchPostById = () =>
     apiClient
-      .get<PostResponse[], Partial<PostQuery>>(postsKey, {
-        authorId,
-        order,
-      })
+      .get<PostResponse>(`posts/${id}`, undefined, { headers: options.headers })
       .then(({ data }) => data);
 
-  return queryOptions<PostResponse[]>({
-    queryKey: [postsKey, filters.authorId, filters.order],
-    queryFn: () => fetchPosts(filters),
+  return queryOptions<PostResponse>({
+    queryKey: [postsKey, { id }],
+    queryFn: () => fetchPostById(),
+  });
+};
+
+export const postsQueryOptions = (
+  query: PostQueryPayload = {},
+  options: { headers?: HeadersInit } = {},
+) => {
+  const fetchPosts = (query: PostQueryPayload) =>
+    apiClient
+      .get<
+        PostQueryResponse,
+        PostQueryPayload
+      >('posts', query, { headers: options.headers })
+      .then(({ data }) => data);
+
+  return queryOptions<PostQueryResponse>({
+    queryKey: [postsKey, query.order, query],
+    queryFn: () => fetchPosts(query),
   });
 };
 
 export const postDeleteMutationOptions = (
-  slug: string,
-  options: { toast?: Toast; onSettled?: () => void } = {},
+  id: string,
+  options: {
+    toast?: Toast;
+    onSuccess?: () => void;
+    onSettled?: () => void;
+  } = {},
 ) => {
-  const deletePost = (slug: string) => apiClient.delete(`posts/${slug}`);
+  const deletePost = (id: string) => apiClient.delete(`posts/${id}`);
   const queryClient = getQueryClient();
-  const toastKey = `${slug}-delete`;
+  const toastKey = `post-${id}-delete`;
 
   return mutationOptions({
-    mutationFn: () => deletePost(slug),
+    mutationFn: () => deletePost(id),
     onMutate: () => options.toast?.({ toastKey, mode: 'loading' }),
     onError: (e) =>
       options.toast?.({
@@ -44,25 +73,29 @@ export const postDeleteMutationOptions = (
       options.toast?.({ toastKey, mode: 'success', content: 'Post deleted' });
       queryClient.setQueriesData<PostResponse[]>(
         { queryKey: [postsKey] },
-        (prev) => prev?.filter((post) => post.slug !== slug),
+        (prev) =>
+          Array.isArray(prev)
+            ? prev.filter((post) => post.id !== id)
+            : undefined,
       );
       // reconcile with server in background
       queryClient.invalidateQueries({ queryKey: [postsKey] });
+      options.onSuccess?.();
     },
     onSettled: options.onSettled,
   });
 };
 
 export const postCreateMutationOptions = (
-  options: { toast?: Toast; onSuccess?: () => void } = {},
+  options: { toast?: Toast; onSuccess?: (post: PostResponse) => void } = {},
 ) => {
-  const createPost = (payload: CreatePost) =>
-    apiClient.post<PostResponse, CreatePost>('/posts', payload);
+  const createPost = (payload: CreatePostPayload) =>
+    apiClient.post<PostResponse, CreatePostPayload>('/posts', payload);
   const queryClient = getQueryClient();
   const toastKey = 'post-create';
 
   return mutationOptions({
-    mutationFn: (payload: CreatePost) =>
+    mutationFn: (payload: CreatePostPayload) =>
       createPost(payload).then(({ data }) => data),
     onMutate: () => options.toast?.({ toastKey, mode: 'loading' }),
     onError: (e) =>
@@ -71,10 +104,76 @@ export const postCreateMutationOptions = (
         mode: 'error',
         content: `Operation failed: ${e.message}`,
       }),
-    onSuccess: () => {
+    onSuccess: (post) => {
       options.toast?.({ toastKey, mode: 'success', content: 'Post created' });
+      queryClient.setQueryData([postsKey, { id: post.id }], post);
       queryClient.invalidateQueries({ queryKey: [postsKey] });
-      options.onSuccess?.();
+      options.onSuccess?.(post);
     },
+  });
+};
+
+export const postUpdateMutationOptions = (
+  id: string,
+  options: { toast?: Toast; onSuccess?: (post: PostResponse) => void } = {},
+) => {
+  const updatePost = (payload: UpdatePostPayload) =>
+    apiClient.put<PostResponse, UpdatePostPayload>(`/posts/${id}`, payload);
+  const queryClient = getQueryClient();
+  const toastKey = 'post-update';
+
+  return mutationOptions({
+    mutationFn: (payload: UpdatePostPayload) =>
+      updatePost(payload).then(({ data }) => data),
+    onMutate: () => options.toast?.({ toastKey, mode: 'loading' }),
+    onError: (e) =>
+      options.toast?.({
+        toastKey,
+        mode: 'error',
+        content: `Operation failed: ${e.message}`,
+      }),
+    onSuccess: (post) => {
+      options.toast?.({ toastKey, mode: 'success', content: 'Post updated' });
+      queryClient.setQueryData([postsKey, { id: post.id }], post);
+      queryClient.invalidateQueries({ queryKey: [postsKey] });
+      options.onSuccess?.(post);
+    },
+  });
+};
+
+export const postUpdatePublicationStatusMutationOptions = (
+  id: string,
+  options: {
+    toast?: Toast;
+    onSuccess?: (post: PostResponse) => void;
+    onSettled?: () => void;
+  } = {},
+) => {
+  const patchPostStatus = (payload: PostStatus) =>
+    apiClient.patch<PostResponse>(`posts/${id}/${payload}`);
+  const queryClient = getQueryClient();
+  const toastKey = `post-${id}-status-update`;
+
+  return mutationOptions({
+    mutationFn: (payload: PostStatus) =>
+      patchPostStatus(payload).then(({ data }) => data),
+    onMutate: () => options.toast?.({ toastKey, mode: 'loading' }),
+    onError: (e) =>
+      options.toast?.({
+        toastKey,
+        mode: 'error',
+        content: `Operation failed: ${e.message}`,
+      }),
+    onSuccess: (post, newStatus) => {
+      options.toast?.({
+        toastKey,
+        mode: 'success',
+        content: `The post has been ${newStatus.toLowerCase()}`,
+      });
+      queryClient.setQueryData([postsKey, { id }], post);
+      queryClient.invalidateQueries({ queryKey: [postsKey] });
+      options.onSuccess?.(post);
+    },
+    onSettled: options.onSettled,
   });
 };
