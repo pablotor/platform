75244a177fdca6813e0f424c8e04368eaa7fcf13
pablotor <me@pablotor.dev>
2026-09-08 20:04:37 +0300

    feat: added preview page

---
+++ apps/web/app/(withNavSidebar)/(authenticated)/dashboard/posts/[id]/preview/page.tsx
@@ -0,0 +1,28 @@
+import { QueryClient } from '@tanstack/react-query';
+import { headers } from 'next/headers';
+
+import PostLayout from '../../../../../../../components/post/postLayout';
+import { postByIdQueryOptions } from '../../../../../../../lib/queryOptions/postsOptions';
+import { getUser } from '../../../../../../../lib/userContext';
+
+const PostPreviewPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  const user = (await getUser()) || undefined;
+  const reqHeaders = await headers();
+  const queryClient = new QueryClient();
+  const post = await queryClient.query(
+    postByIdQueryOptions(id, { headers: reqHeaders }),
+  );
+
+  return (
+    <main>
+      <PostLayout {...post} author={user} />
+    </main>
+  );
+};
+
+export default PostPreviewPage;
